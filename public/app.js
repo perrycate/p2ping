da706390cb1eb0fc8321904b796c5d0577b07836
Perry Cate <ncate@alumni.princeton.edu>
2020-09-16 21:37:21 -0700

    Create Connection object to encapsulate connection-related state.
    This will make it easier to have multiple connections later. Besides,
    global variables bad.
    
    Also deleted unused hangUp code while I was at it.
    
    Squashed commit of the following:
    
    commit 9148954f90d104126423e0d8be8a0cb72af005dd
    Author: Perry Cate <ncate@alumni.princeton.edu>
    Date:   Wed Sep 16 21:33:54 2020 -0700
    
        Move registerPeerConnectionListeners() to Connection object.
    
    commit e8798ffb175107167e462c2d88c533c87980aa5c
    Author: Perry Cate <ncate@alumni.princeton.edu>
    Date:   Wed Sep 16 21:26:53 2020 -0700
    
        Delete hangup button.
    
        It was disabled anyway.
    
    commit 5fd94d05ba51ece2cd2f9d5477cc6a22292c9301
    Author: Perry Cate <ncate@alumni.princeton.edu>
    Date:   Wed Sep 16 21:24:25 2020 -0700
    
        Move joinById function into connection.
    
    commit 0d511dcf8fe2c95876687dca0de999af61232930
    Author: Perry Cate <ncate@alumni.princeton.edu>
    Date:   Tue Sep 15 19:56:43 2020 -0700
    
        Change references from conn to this within Connection.create() method.
    
    commit e9e7befe8ca4f4d24bc7bc29edf8b663352bf077
    Author: Perry Cate <ncate@alumni.princeton.edu>
    Date:   Mon Sep 14 21:17:35 2020 -0700
    
        Move init() to bottom of file.
    
    commit c625ad1a137ebd2ac83901809d77b5926c6f5f0f
    Author: Perry Cate <ncate@alumni.princeton.edu>
    Date:   Mon Sep 14 21:15:14 2020 -0700
    
        Move create() function into Connection class.
    
    commit 870fa997b84d7d5f5f176ba557fa6f00603497c0
    Author: Perry Cate <ncate@alumni.princeton.edu>
    Date:   Mon Sep 14 20:52:32 2020 -0700
    
        Remove references to "rooms".
    
        Obsolete since this codebase no longer deals with chat/video rooms.
    
    commit db57dafc6df527137077d8f600f04873567be7dd
    Author: Perry Cate <ncate@alumni.princeton.edu>
    Date:   Sun Sep 13 15:05:06 2020 -0700
    
        Replace globals with a single object containing those things.
    
        First step on the way to modularizing this stuff.

--- public/app.js
+++ public/app.js
@@ -12,207 +12,180 @@ const configuration = {
   iceCandidatePoolSize: 10,
 };
 
-let peerConnection = null;
-let dataChannel = null;
-let roomDialog = null;
-let roomId = null;
-
-function init() {
-  document.querySelector('#hangupBtn').addEventListener('click', hangUp);
-  document.querySelector('#createBtn').addEventListener('click', createRoom);
-  if (window.location.pathname != "/") {
-    document.querySelector('#createBtn').disabled = true;
-    document.querySelector('#latency').innerText = "Connecting..."
-    joinRoomById(window.location.pathname.slice(1));
+class Connection {
+  constructor() {
+    this.peerConnection = null;
+    this.dataChannel = null;
+    this.roomId = null;
   }
-}
+  async create() {
+    document.querySelector('#createBtn').disabled = true;
+    const db = firebase.firestore();
+    const a = await db.collection('conns');
+    console.log(a);
+    const roomRef = await db.collection('conns').doc();
 
-async function createRoom() {
-  document.querySelector('#createBtn').disabled = true;
-  const db = firebase.firestore();
-  const a = await db.collection('rooms');
-  console.log(a);
-  const roomRef = await db.collection('rooms').doc();
-
-  console.log('Create PeerConnection with configuration: ', configuration);
-  peerConnection = new RTCPeerConnection(configuration);
-
-  dataChannel = peerConnection.createDataChannel("test");
-  console.log("test");
-  dataChannel.addEventListener("open", event => {
-    console.log("open!");
-    setInterval(() => dataChannel.send(Date.now()), 1000);
-  });
-  dataChannel.addEventListener('message', event => {
-    const sentTime = parseInt(event.data);
-    elapsedMs = Date.now() - sentTime;
-    console.log(elapsedMs + " elapsed!")
-    document.querySelector('#latency').innerText = `Your round trip latency in ms: ${elapsedMs}`;
-  });
-  registerPeerConnectionListeners();
-
-  // Collect ICE Candidates for the current browser.
-  const callerCandidatesCollection = roomRef.collection('callerCandidates');
-  peerConnection.addEventListener('icecandidate', event => {
-    if (!event.candidate) {
-      console.log('Got final candidate!');
-      return;
-    }
-    console.log('Got candidate: ', event.candidate);
-    callerCandidatesCollection.add(event.candidate.toJSON());
-  });
-
-  // Create p2p "room".
-  const offer = await peerConnection.createOffer();
-  await peerConnection.setLocalDescription(offer);
-  console.log('Created offer:', offer);
-  const roomWithOffer = {
-    'offer': {
-      type: offer.type,
-      sdp: offer.sdp,
-    },
-  };
-  await roomRef.set(roomWithOffer);
-  roomId = roomRef.id;
-  console.log(`New room created with SDP offer. Room ID: ${roomRef.id}`);
-  document.querySelector('#urlDisplay').innerText = `Share this link (keep this tab open) to test your peer-to-peer latency: ${new URL(roomRef.id, window.location)}`;
-
-  // Listening for remote session description.
-  roomRef.onSnapshot(async snapshot => {
-    const data = snapshot.data();
-    if (!peerConnection.currentRemoteDescription && data && data.answer) {
-      console.log('Got remote description: ', data.answer);
-      const rtcSessionDescription = new RTCSessionDescription(data.answer);
-      await peerConnection.setRemoteDescription(rtcSessionDescription);
-    }
-  });
-
-  // Listen for remote ICE candidates.
-  roomRef.collection('calleeCandidates').onSnapshot(snapshot => {
-    snapshot.docChanges().forEach(async change => {
-      if (change.type === 'added') {
-        let data = change.doc.data();
-        console.log(`Got new remote ICE candidate: ${JSON.stringify(data)}`);
-        await peerConnection.addIceCandidate(new RTCIceCandidate(data));
-      }
-    });
-  });
-}
-
-async function joinRoomById(roomId) {
-  const db = firebase.firestore();
-  const roomRef = db.collection('rooms').doc(`${roomId}`);
-  const roomSnapshot = await roomRef.get();
-  console.log('Got room:', roomSnapshot.exists);
-
-  if (roomSnapshot.exists) {
     console.log('Create PeerConnection with configuration: ', configuration);
-    peerConnection = new RTCPeerConnection(configuration);
-    console.log("created connection");
+    this.peerConnection = new RTCPeerConnection(configuration);
 
-    registerPeerConnectionListeners();
+    this.dataChannel = this.peerConnection.createDataChannel("test");
+    console.log("test");
+    this.dataChannel.addEventListener("open", event => {
+      console.log("open!");
+      setInterval(() => this.dataChannel.send(Date.now()), 1000);
+    });
+    this.dataChannel.addEventListener('message', event => {
+      const sentTime = parseInt(event.data);
+      let elapsedMs = Date.now() - sentTime;
+      console.log(elapsedMs + " elapsed!")
+      document.querySelector('#latency').innerText = `Your round trip latency in ms: ${elapsedMs}`;
+    });
+    this.registerPeerConnectionListeners();
 
-    // Collect ICE candidates.
-    const calleeCandidatesCollection = roomRef.collection('calleeCandidates');
-    peerConnection.addEventListener('icecandidate', event => {
+    // Collect ICE Candidates for the current browser.
+    const callerCandidatesCollection = roomRef.collection('callerCandidates');
+    this.peerConnection.addEventListener('icecandidate', event => {
       if (!event.candidate) {
         console.log('Got final candidate!');
         return;
       }
       console.log('Got candidate: ', event.candidate);
-      calleeCandidatesCollection.add(event.candidate.toJSON());
+      callerCandidatesCollection.add(event.candidate.toJSON());
     });
 
-    // Create SDP answer.
-    const offer = roomSnapshot.data().offer;
-    console.log('Got offer:', offer);
-    await peerConnection.setRemoteDescription(new RTCSessionDescription(offer));
-    const answer = await peerConnection.createAnswer();
-    console.log('Created answer:', answer);
-    await peerConnection.setLocalDescription(answer);
-
-    const roomWithAnswer = {
-      answer: {
-        type: answer.type,
-        sdp: answer.sdp,
+    // Create p2p "room".
+    const offer = await this.peerConnection.createOffer();
+    await this.peerConnection.setLocalDescription(offer);
+    console.log('Created offer:', offer);
+    const roomWithOffer = {
+      'offer': {
+        type: offer.type,
+        sdp: offer.sdp,
       },
     };
-    await roomRef.update(roomWithAnswer);
+    await roomRef.set(roomWithOffer);
+    this.roomId = roomRef.id;
+    console.log(`New room created with SDP offer. Room ID: ${roomRef.id}`);
+    document.querySelector('#urlDisplay').innerText = `Share this link (keep this tab open) to test your peer-to-peer latency: ${new URL(roomRef.id, window.location)}`;
+
+    // Listening for remote session description.
+    roomRef.onSnapshot(async snapshot => {
+      const data = snapshot.data();
+      if (!this.peerConnection.currentRemoteDescription && data && data.answer) {
+        console.log('Got remote description: ', data.answer);
+        const rtcSessionDescription = new RTCSessionDescription(data.answer);
+        await this.peerConnection.setRemoteDescription(rtcSessionDescription);
+      }
+    });
 
     // Listen for remote ICE candidates.
-    roomRef.collection('callerCandidates').onSnapshot(snapshot => {
+    roomRef.collection('calleeCandidates').onSnapshot(snapshot => {
       snapshot.docChanges().forEach(async change => {
         if (change.type === 'added') {
           let data = change.doc.data();
           console.log(`Got new remote ICE candidate: ${JSON.stringify(data)}`);
-          await peerConnection.addIceCandidate(new RTCIceCandidate(data));
+          await this.peerConnection.addIceCandidate(new RTCIceCandidate(data));
         }
       });
     });
-
-
-    peerConnection.addEventListener('datachannel', event => {
-      console.log("recieved channel!");
-      dataChannel = event.channel;
-      dataChannel.addEventListener('message', event => {
-        const msg = event.data;
-        console.log("got: " + msg);
-        dataChannel.send(msg);
-      })
-    });
-
-    document.querySelector('#latency').innerText = "Connected! Your peer is measuring latency now."
-  }
-}
-
-async function hangUp(e) {
-
-  if (peerConnection) {
-    peerConnection.close();
   }
 
-  document.querySelector('#cameraBtn').disabled = false;
-  document.querySelector('#joinBtn').disabled = true;
-  document.querySelector('#createBtn').disabled = true;
-  document.querySelector('#hangupBtn').disabled = true;
-  document.querySelector('#currentRoom').innerText = '';
-
-  // Delete room on hangup.
-  if (roomId) {
+  async joinById(roomId) {
+    console.log('Getting room with id: ', roomId)
     const db = firebase.firestore();
-    const roomRef = db.collection('rooms').doc(roomId);
-    const calleeCandidates = await roomRef.collection('calleeCandidates').get();
-    calleeCandidates.forEach(async candidate => {
-      await candidate.ref.delete();
-    });
-    const callerCandidates = await roomRef.collection('callerCandidates').get();
-    callerCandidates.forEach(async candidate => {
-      await candidate.ref.delete();
-    });
-    await roomRef.delete();
+    const roomRef = db.collection('conns').doc(`${roomId}`);
+    const roomSnapshot = await roomRef.get();
+    console.log('Got room:', roomSnapshot.exists);
+
+    if (roomSnapshot.exists) {
+      console.log('Create PeerConnection with configuration: ', configuration);
+      this.peerConnection = new RTCPeerConnection(configuration);
+      console.log("created connection");
+
+      this.registerPeerConnectionListeners();
+
+      // Collect ICE candidates.
+      const calleeCandidatesCollection = roomRef.collection('calleeCandidates');
+      this.peerConnection.addEventListener('icecandidate', event => {
+        if (!event.candidate) {
+          console.log('Got final candidate!');
+          return;
+        }
+        console.log('Got candidate: ', event.candidate);
+        calleeCandidatesCollection.add(event.candidate.toJSON());
+      });
+
+      // Create SDP answer.
+      const offer = roomSnapshot.data().offer;
+      console.log('Got offer:', offer);
+      await this.peerConnection.setRemoteDescription(new RTCSessionDescription(offer));
+      const answer = await this.peerConnection.createAnswer();
+      console.log('Created answer:', answer);
+      await this.peerConnection.setLocalDescription(answer);
+
+      const roomWithAnswer = {
+        answer: {
+          type: answer.type,
+          sdp: answer.sdp,
+        },
+      };
+      await roomRef.update(roomWithAnswer);
+
+      // Listen for remote ICE candidates.
+      roomRef.collection('callerCandidates').onSnapshot(snapshot => {
+        snapshot.docChanges().forEach(async change => {
+          if (change.type === 'added') {
+            let data = change.doc.data();
+            console.log(`Got new remote ICE candidate: ${JSON.stringify(data)}`);
+            await this.peerConnection.addIceCandidate(new RTCIceCandidate(data));
+          }
+        });
+      });
+
+      this.peerConnection.addEventListener('datachannel', event => {
+        console.log("recieved channel!");
+        this.dataChannel = event.channel;
+        this.dataChannel.addEventListener('message', event => {
+          const msg = event.data;
+          console.log("got: " + msg);
+          this.dataChannel.send(msg);
+        })
+      });
+
+      document.querySelector('#latency').innerText = "Connected! Your peer is measuring latency now."
+    }
   }
 
-  document.location.reload(true);
+  registerPeerConnectionListeners() {
+    this.peerConnection.addEventListener('icegatheringstatechange', () => {
+      console.log(
+        `ICE gathering state changed: ${this.peerConnection.iceGatheringState}`);
+    });
+
+    this.peerConnection.addEventListener('connectionstatechange', () => {
+      console.log(`Connection state change: ${this.peerConnection.connectionState}`);
+    });
+
+    this.peerConnection.addEventListener('signalingstatechange', () => {
+      console.log(`Signaling state change: ${this.peerConnection.signalingState}`);
+    });
+
+    this.peerConnection.addEventListener('iceconnectionstatechange ', () => {
+      console.log(
+        `ICE connection state change: ${this.peerConnection.iceConnectionState}`);
+    });
+  }
 }
 
-function registerPeerConnectionListeners() {
-  peerConnection.addEventListener('icegatheringstatechange', () => {
-    console.log(
-      `ICE gathering state changed: ${peerConnection.iceGatheringState}`);
-  });
+let conn = new Connection();
 
-  peerConnection.addEventListener('connectionstatechange', () => {
-    console.log(`Connection state change: ${peerConnection.connectionState}`);
-  });
-
-  peerConnection.addEventListener('signalingstatechange', () => {
-    console.log(`Signaling state change: ${peerConnection.signalingState}`);
-  });
-
-  peerConnection.addEventListener('iceconnectionstatechange ', () => {
-    console.log(
-      `ICE connection state change: ${peerConnection.iceConnectionState}`);
-  });
+function init() {
+  document.querySelector('#createBtn').addEventListener('click', conn.create.bind(conn));
+  if (window.location.pathname != "/") {
+    document.querySelector('#createBtn').disabled = true;
+    document.querySelector('#latency').innerText = "Connecting..."
+    conn.joinById(window.location.pathname.slice(1));
+  }
 }
 
 init();
